bd5099bdaa6e6c8686b50c1c64b23137b562bf2b
Adri <adriana.ovando@ucb.edu.bo>
2023-11-13 16:01:57 -0400

    subir tienda nueva

--- src/app/paginas/productos/producto/producto.component.ts
+++ src/app/paginas/productos/producto/producto.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,EventEmitter,Input,Output } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { Producto } from 'src/app/producto';
+import { ProductosService } from 'src/app/servicios/productos.service';
+
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.scss']
 })
-export class ProductoComponent {
+export class ProductoComponent implements  OnInit {
+  @Input() producto?: Producto;
 
+  constructor(private route: ActivatedRoute ,
+    private productosService: ProductosService,
+    private location: Location) {}
+
+  ngOnInit() {
+    this.getProduct();
+  }
+  goBack(): void {
+    this.location.back();
+  }
+  getProduct(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productosService.obtenerUnProductoporId(id)
+      .subscribe(producto => this.producto = producto);
+  }
 }
